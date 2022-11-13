c7d3f8f6418779960efe424a6ce2d021eb4f9cc7
harshal <harshalc562@gmail.com>
2022-11-14 00:50:18 +0530

    adjustedintendation

--- Basic/Objectdemo.java
+++ Basic/Objectdemo.java
@@ -3,21 +3,19 @@ class Calc{
     int m;
     int result;
      
-        public void perform(){
-            result = m + n;
+            public void perform(){
+                result = m + n;
         }
 }
 public class Objectdemo {
     public static void main(String[] args) {
         
     
-    Calc obj = new Calc();
+        Calc obj = new Calc();
         obj.n = 76;
         obj.m = 53;
 
         obj.perform();
         System.out.println(obj.result);
-
-
     }
 }
