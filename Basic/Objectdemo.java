c357f4b9e8ff7556e6bcd52f4f382f7bdccf3b01
harshal <harshalc562@gmail.com>
2022-11-14 00:21:27 +0530

    Objectcode

---
+++ Basic/Objectdemo.java
@@ -0,0 +1,23 @@
+class Calc{
+    int n;
+    int m;
+    int result;
+     
+        public void perform(){
+            result = m + n;
+        }
+}
+public class Objectdemo {
+    public static void main(String[] args) {
+        
+    
+    Calc obj = new Calc();
+        obj.n = 76;
+        obj.m = 53;
+
+        obj.perform();
+        System.out.println(obj.result);
+
+
+    }
+}
