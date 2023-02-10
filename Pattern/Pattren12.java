bde93bad851e47f46b64158c05f7c87c088133ad
harshal <harshalc562@gmail.com>
2023-02-11 01:15:09 +0530

    pATTREN12

---
+++ Pattern/Pattren12.java
@@ -0,0 +1,16 @@
+public class Pattren12 {
+    public static void main(String[] args) {
+        int n = 5;
+        int a = 0;
+        int b =1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(a + "\t");
+                int c = a + b;
+                a = b;
+                b = c;
+            }
+            System.out.println(" ");
+        }
+    }
+}
