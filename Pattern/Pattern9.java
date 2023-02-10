301f6ad5d0e4cf6d036f1f355927f97454338b60
harshal <harshalc562@gmail.com>
2023-02-11 00:27:04 +0530

    Pattern.java

---
+++ Pattern/Pattern9.java
@@ -0,0 +1,16 @@
+public class Pattern9{
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(i + j == n + 1 || i == j){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
