29a8fafb2b6ac1546ae89ffc9b6dc4add30030bf
harshal <harshalc562@gmail.com>
2023-02-10 23:09:47 +0530

    Pattern5

--- Pattern/Pattern5.java
+++ Pattern/Pattern5.java
@@ -3,15 +3,20 @@ public class Pattern5 {
         int n = 5;
         int sp = n / 2;
         int st = 1;
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= sp; j++){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sp; j++) {
                 System.out.print("\t");
             }
-            for(int j = 1; i <= st; j++){
-                System.out.println("*");
+            for (int j = 1; j <= st; j++) {
+                System.out.print("*\t");
             }
-            sp--;
-            st += 2;
+            if (i <= n / 2) {
+                sp--;
+                st += 2;
+            } else {
+                sp++;
+                st -= 2;
+            }
             System.out.println();
         }
     }
